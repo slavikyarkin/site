a84907355aa864cfb5d17755a2d2b14f1755ad4c
Slavik Yarkin <slavikyarkin@gmail.com>
2017-07-26 21:19:00 +0300

    ispravil

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,17 @@
-'layouts.app'
+@extends('layouts.app')
 
-'content'
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in!
+                    <div class="panel-body">
+                        You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
